b049ca8509624d8e9b00af1573df0d9bc803d01f
albaihaqiMF <241099+albaihaqiMF@users.noreply.github.com>
2021-11-18 16:47:05 +0700

    forelse

--- resources/views/livewire/doctor-list.blade.php
+++ resources/views/livewire/doctor-list.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </div>
     <div class="mt-5">
-        @foreach ($data as $item)
+        @forelse ($data as $item)
         <div class="intro-x">
             <div class="box px-5 py-3 mb-3 flex items-center zoom-in">
                 <div class="w-10 h-10 flex-none image-fit rounded-full overflow-hidden">
@@ -18,7 +18,13 @@
                 <div class="text-theme-20">{{ $item->role->name }}</div>
             </div>
         </div>
-        @endforeach
+        @empty
+        <div class="intro-x">
+            <div class="flex justify-center">
+                Data Kosong
+            </div>
+        </div>
+        @endforelse
 
         <a href="" class="intro-y w-full block text-center rounded-md py-3 border border-dotted border-theme-32 dark:border-dark-5 text-theme-33 dark:text-gray-600">View More</a>
     </div>
